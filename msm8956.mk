43f1c8f5042177aa8f853e3a2cdeb509cc3751f3
Alex Yustasov <alex9yust@gmail.com>
2018-03-17 01:03:23 +0300

    add XiaomiParts, XiaomiDoze

--- msm8956.mk
+++ msm8956.mk
@@ -325,3 +325,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+# Doze mode
+PRODUCT_PACKAGES += \
+    XiaomiDoze
+
+#XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
